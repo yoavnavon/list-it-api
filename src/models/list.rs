40358322bacaabfa5b55b7974b9409a99c3c67da
Yoav Navon <yoavnavon77@gmail.com>
2023-09-07 17:28:00 -0700

    Handle complex content

--- src/models/list.rs
+++ src/models/list.rs
@@ -2,12 +2,19 @@ use mongodb::bson::oid::ObjectId;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct ListItem {
-    pub text: String,
-    pub item_type: Option<String>,
+#[serde(tag = "item_type", rename_all = "lowercase")]
+pub enum Content {
+    Text { text: String },
+    Image { url: String },
+    List { list: List },
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct ListItem {
+    pub content: Content,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct List {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub _id: Option<ObjectId>,
